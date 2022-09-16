122f338fb361cc9ad58c68b92c18918bb44d5558
yash ranjan <yashranjan@yashs-MacBook-Air.local>
2022-09-16 17:25:33 -0400

    Add the mining part of the code.
    The miner will start finding the hash from a starting
    number and if the hash of the number cotains K zeros
    in the beginning, it will return the Hash and number to
    the master.
    
    Signed-off-by: yash ranjan <yashranjan@ufl.edu>

--- master.erl
+++ master.erl
@@ -1,26 +1,20 @@
 -module(master).
 -export([start/0]).
 
-% found_coin() ->
-%     receive
-%         { Coin, Sender_id} ->
-%             io:fwrite(" Mine minted ~p from miner ~p\n", [Coin, Sender_id]),
-%             found_coin()
-%     end.
-
-wait_for_miner() ->
+wait_for_miner(Count, Zcount) ->
     receive %% waiting for a miner to connect
         { Sender_id_1 }-> 
-            Sender_id_1 ! { 1000 }, %% sending back the start variable
+            Sender_id_1 ! { Count, Zcount }, %% sending back the start string and number of zeros in hash
             io:fwrite("Recived connection from ~p\n", [Sender_id_1]),
-            wait_for_miner(); %% waiting for another miner
+            wait_for_miner(Count, Zcount); %% waiting for another miner
 
-        { Coin, Sender_id_2} ->
-            io:fwrite(" Minted ~p from miner ~p\n", [Coin, Sender_id_2]),
-            wait_for_miner()
+        { Coin, Newcount, Sender_id_2} ->
+            io:fwrite(" Minted ~p with count ~p from miner ~p\n", [Coin, Newcount, Sender_id_2]),
+            wait_for_miner(Newcount, Zcount)
     end.
     
 
 start() ->
-    Pid = spawn(fun() -> wait_for_miner() end),
+    {ok, Zcount} = io:read("Number of 0s to mine: "),
+    Pid = spawn(fun() -> wait_for_miner(0, Zcount) end),
     register(master, Pid).
--- miner.erl
+++ miner.erl
@@ -1,16 +1,26 @@
 -module(miner).
+-import(string,[substr/3, right/3, concat/2]).
 -export([start/0]).
 
-start_mining(Num) -> % start mining
-    io:fwrite("Started mining from ~p\n", [Num]),
-    master ! {Num, self()}.
-
-    % start_mining(Num).    Keep mining
+start_mining(Count, Zcount) -> % start mining
+    Name = "shubhamagiwal92;",
+    String = integer_to_list(Count),
+    Hashstring = concat(Name, String),
+    Hash = io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256, Hashstring))]),
+    Zerostring = right("", Zcount, $0),
+    Substring = substr(Hash, 1, Zcount),
+    if
+        Zerostring == Substring ->
+            master ! {Hash, Count, self()};
+    true ->
+        io:fwrite("")
+    end,
+    start_mining(Count+1, Zcount).
 
 connect_to_master() ->
     master ! {self()},
     receive
-        { Num } -> start_mining(Num)
+        { String, Zcount } -> start_mining(String, Zcount)
     end.
 
 
