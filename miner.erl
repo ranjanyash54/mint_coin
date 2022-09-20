671bc8a463a19da4b0b1b63ee8b55cd2fe4d1fbb
Yash Ranjan <yashranjan@ufl.edu>
2022-09-20 14:04:13 -0400

    Match the exact number of Zeros. If the hash has more or less than K, then it won't be minted.
    Signed-off-by: Yash Ranjan <yashranjan@ufl.edu>

--- miner.erl
+++ miner.erl
@@ -2,26 +2,28 @@
 -import(string,[substr/3, right/3, concat/2]).
 -export([start/1]).
 
-start_mining(Count, Zcount) -> % start mining
+start_mining(Count, Zcount, Masteraddress) -> % start mining
     Name = "shubhamagiwal92;",
     String = integer_to_list(Count),
     Hashstring = concat(Name, String),
     Hash = io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256, Hashstring))]),
     Zerostring = right("", Zcount, $0),
+    Maxzerostring = right("", Zcount+1, $0),
     Substring = substr(Hash, 1, Zcount),
+    Maxsubstring = substr(Hash, 1, Zcount+1),
     if
-        Zerostring == Substring ->
-            {master, 'master@127.0.0.1'} ! {Hash, Count, self()};
+        (Zerostring == Substring) and (Maxzerostring =/= Maxsubstring)->
+            {master, Masteraddress} ! {Hash, Count, self()};
     true ->
         io:fwrite("")
     end,
-    start_mining(Count+1, Zcount).
+    start_mining(Count+1, Zcount, Masteraddress).
 
 connect_to_master(Ipaddress) ->
     Masteraddress = list_to_atom(concat("master", concat("@", Ipaddress))),
     {master, Masteraddress} ! {self()},
     receive
-        { String, Zcount } -> start_mining(String, Zcount)
+        { String, Zcount } -> start_mining(String, Zcount, Masteraddress)
     end.
 
 
