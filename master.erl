5d179c4275d6e830c0afab299ad985d7ad5e9919
rsrivastav54 <70634373+rsrivastav54@users.noreply.github.com>
2022-09-20 17:55:57 -0400

    updated master.erl

--- master.erl
+++ master.erl
@@ -1,5 +1,6 @@
 -module(master).
 -export([start/0]).
+-import(string,[substr/3, right/3, concat/2]).
 
 wait_for_miner(Zcount) ->
     receive %% waiting for a miner to connect
@@ -17,4 +18,24 @@ wait_for_miner(Zcount) ->
 start() ->
     {ok, Zcount} = io:read("Number of 0s to mine: "),
     Pid = spawn(fun() -> wait_for_miner(Zcount) end),
+    start_mining(Zcount),
     register(master, Pid).
+
+
+start_mining(Zcount) -> % start mining
+    Name = "shubhamagiwal92;",
+    % String = integer_to_list(Count),
+    String = base64:encode_to_string(crypto:strong_rand_bytes(100)),
+    Hashstring = concat(Name, String),
+    Hash = io_lib:format("~64.16.0b", [binary:decode_unsigned(crypto:hash(sha256, Hashstring))]),
+    Zerostring = right("", Zcount, $0),
+    Maxzerostring = right("", Zcount+1, $0),
+    Substring = substr(Hash, 1, Zcount),
+    Maxsubstring = substr(Hash, 1, Zcount+1),
+    if
+        (Zerostring == Substring) and (Maxzerostring =/= Maxsubstring)->
+        io:fwrite(" Minted ~p with string ~p from server \n", [Hash, Hashstring]);
+    true ->
+        ok
+    end,
+    start_mining(Zcount).
